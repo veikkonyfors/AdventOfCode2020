fb91ecf6e2d97db8d1fb280929e8e88f2de46d93
Veikko Nyfors <veikkonyfors@gmail.com>
2022-11-03 12:04:40 +0200

    Amend advent2020day3traversetrees to cover day's second puzzle.

--- app/advent2020day3traversetrees/README.md
+++ app/advent2020day3traversetrees/README.md
@@ -1,5 +1,5 @@
 # Advent of Code 2020 day 3
 
-Solution for day's first puzzle.
+Solution for day's first and second puzzle.
 
 One needs to store puzzle input into   File(applicationContext.filesDir, "adv2020_trees.txt").
--- app/advent2020day3traversetrees/src/main/java/com/viware/advent2020day3traversetrees/MainActivity.kt
+++ app/advent2020day3traversetrees/src/main/java/com/viware/advent2020day3traversetrees/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import java.io.File
+import java.math.BigInteger
 
 private const val TAG = "MainActivity"
 
@@ -22,7 +23,22 @@ class MainActivity : AppCompatActivity() {
             listOfContents = file.readLines()
 
             val traverser=Traverser(listOfContents)
-            findViewById<TextView>(R.id.textView).setText(traverser.getNumTrees().toString())
+            // First puzzle
+            findViewById<TextView>(R.id.textViewNumTree).setText(traverser.getNumTrees(3,1).toString())
+            // Second puzzle
+            val c11=traverser.getNumTrees(1,1).toBigInteger()
+            val c31=traverser.getNumTrees(3,1).toBigInteger()
+            val c51=traverser.getNumTrees(5,1).toBigInteger()
+            val c71=traverser.getNumTrees(7,1).toBigInteger()
+            val c12=traverser.getNumTrees(1,2).toBigInteger()
+            val product:BigInteger=c11*c31*c51*c71*c12
+            /*Log.i(TAG,
+                "In checkButton setOnClickListener lambda, c11, c31, c51, c71, c12, product: "+
+                        " $c11, $c31, $c51, $c71, $c12, $product")*/
+
+            findViewById<TextView>(R.id.textViewProduct).setText(product.toString())
+
+            //Log.i(TAG,"In checkButton setOnClickListener lambda, product: "+product)
         }
     }
 
@@ -46,6 +62,25 @@ class MainActivity : AppCompatActivity() {
                 c+=3
                 i=(c)%listOfTrees[index].length
                 if(listOfTrees[index][i]=='#') t++
+            }
+            return t
+        }
+
+        fun getNumTrees(right:Int, down:Int): Int {
+
+            val toRight=right
+            val toDown=down
+
+            var c=0
+            var t=0
+            var i:Int
+
+            for(index in toDown..listOfTrees.size-1 step toDown){
+                c+=right
+                i=(c)%listOfTrees[index].length
+                if(listOfTrees[index][i]=='#') t++
+                //println("$index, $i, $t, "+listOfTrees[index][i]+", \t"+listOfTrees[index])
+                //println("\t\t\t\t012345678901234567890123456789")
             }
             return t
         }
